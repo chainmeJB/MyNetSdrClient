a7c2647fa796990413b8993e619b170eb0d3608c
Юлия Бенедюк <crimsoncommander21@gmail.com>
2025-10-31 19:07:21 +0200

    test: Add 3 architecture tests

---
+++ NetSdrClientAppTests/ArchitectureTests.cs
@@ -0,0 +1,47 @@
+﻿using NetArchTest.Rules;
+using NetSdrClientApp.Messages;
+
+namespace NetSdrClientTests
+{
+    public class ArchitectureTests
+    {
+        [Test]
+        public void Messages_Should_Not_HaveDependencyOn_Networking()
+        {
+            var result = Types.InAssembly(typeof(NetSdrMessageHelper).Assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp.Messages")
+                .ShouldNot()
+                .HaveDependencyOn("NetSdrClientApp.Networking")
+                .GetResult();
+
+            Assert.IsTrue(result.IsSuccessful, $"Messages depend on Networking: {string.Join(", ", result.FailingTypeNames ?? Array.Empty<string>())}");
+        }
+
+        [Test]
+        public void Networking_Should_Not_HaveDependencyOn_Messages()
+        {
+            var result = Types.InAssembly(typeof(NetSdrMessageHelper).Assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp.Networking")
+                .ShouldNot()
+                .HaveDependencyOn("NetSdrClientApp.Messages")
+                .GetResult();
+
+            Assert.IsTrue(result.IsSuccessful, $"Networking depends on Messages: {string.Join(", ", result.FailingTypeNames ?? Array.Empty<string>())}");
+        }
+
+        [Test]
+        public void Messages_Should_Not_HaveDependencyOn_EchoTspServer()
+        {
+            var result = Types.InAssembly(typeof(NetSdrMessageHelper).Assembly)
+                .That()
+                .ResideInNamespace("NetSdrClientApp.Messages")
+                .ShouldNot()
+                .HaveDependencyOn("EchoTspServer")
+                .GetResult();
+
+            Assert.IsTrue(result.IsSuccessful, $"Messages depend on EchoTspServer: {string.Join(", ", result.FailingTypeNames ?? Array.Empty<string>())}");
+        }
+    }
+}
