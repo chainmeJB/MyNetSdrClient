27564f47e72f0f82f071d62de3665f1c74825f5d
Yulia Benediuk <crimsoncommander21@gmail.com>
2025-11-11 04:13:19 +0200

    refactor: Split EchoServer class on many classes and interfaces

---
+++ EchoTspServer/NetworkStreamWrapper.cs
@@ -0,0 +1,35 @@
+﻿using EchoServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoServer
+{
+    public class NetworkStreamWrapper : INetworkStreamWrapper
+    {
+        private readonly NetworkStream _stream;
+
+        public NetworkStreamWrapper(NetworkStream stream)
+        {
+            _stream = stream;
+        }
+
+        public Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken token)
+        {
+            return _stream.ReadAsync(buffer, offset, count, token);
+        }
+
+        public Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken token)
+        {
+            return _stream.WriteAsync(buffer, offset, count, token);
+        }
+
+        public void Dispose()
+        {
+            _stream?.Dispose();
+        }
+    }
+}
---
+++ EchoTspServer/UdpTimedSender.cs
@@ -0,0 +1,90 @@
+﻿using EchoServer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoServer
+{
+    public class UdpTimedSender : IUdpSender
+    {
+        private readonly string _host;
+        private readonly int _port;
+        private readonly ILogger _logger;
+        private readonly UdpClient _udpClient;
+        private Timer _timer;
+        private ushort _sequenceNumber = 0;
+
+        public UdpTimedSender(string host, int port, ILogger logger = null)
+        {
+            _host = host;
+            _port = port;
+            _logger = logger ?? new ConsoleLogger();
+            _udpClient = new UdpClient();
+        }
+
+        public void StartSending(int intervalMilliseconds)
+        {
+            if (_timer != null)
+                throw new InvalidOperationException("Sender is already running.");
+
+            _timer = new Timer(SendMessageCallback, null, 0, intervalMilliseconds);
+        }
+
+        private void SendMessageCallback(object state)
+        {
+            try
+            {
+                Random rnd = new Random();
+                byte[] samples = new byte[1024];
+                rnd.NextBytes(samples);
+                _sequenceNumber++;
+
+                byte[] msg = CombineArrays(
+                    new byte[] { 0x04, 0x84 },
+                    BitConverter.GetBytes(_sequenceNumber),
+                    samples
+                );
+
+                var endpoint = new IPEndPoint(IPAddress.Parse(_host), _port);
+                _udpClient.Send(msg, msg.Length, endpoint);
+                _logger.Log($"Message sent to {_host}:{_port}");
+            }
+            catch (Exception ex)
+            {
+                _logger.Log($"Error sending message: {ex.Message}");
+            }
+        }
+
+        private byte[] CombineArrays(params byte[][] arrays)
+        {
+            int totalLength = 0;
+            foreach (var arr in arrays)
+                totalLength += arr.Length;
+
+            byte[] result = new byte[totalLength];
+            int offset = 0;
+            foreach (var arr in arrays)
+            {
+                Buffer.BlockCopy(arr, 0, result, offset, arr.Length);
+                offset += arr.Length;
+            }
+            return result;
+        }
+
+        public void StopSending()
+        {
+            _timer?.Dispose();
+            _timer = null;
+        }
+
+        public void Dispose()
+        {
+            StopSending();
+            _udpClient?.Dispose();
+        }
+    }
+}
