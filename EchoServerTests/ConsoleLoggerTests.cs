89c4c0bcd25f88bcec6da4bbf4216202bb6170c3
Yulia Benediuk <crimsoncommander21@gmail.com>
2025-11-11 04:46:56 +0200

    test: Add unit tests for EchoServer

---
+++ EchoServerTests/ConsoleLoggerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EchoServer;
+
+namespace EchoServerTests
+{
+    [TestFixture]
+    public class ConsoleLoggerTests
+    {
+        [Test]
+        public void Log_ShouldNotThrow()
+        {
+            // Arrange
+            var logger = new ConsoleLogger();
+
+            // Act & Assert
+            Assert.DoesNotThrow(() => logger.Log("Test message"));
+        }
+    }
+}
---
+++ EchoServerTests/TcpClientWrapperTests.cs
@@ -0,0 +1,39 @@
+﻿using EchoServer;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EchoServerTests
+{
+    [TestFixture]
+    public class TcpClientWrapperTests
+    {
+        [Test]
+        public void GetStream_ShouldReturnNetworkStreamWrapper()
+        {
+            // Arrange
+            var tcpClient = new TcpClient();
+            var wrapper = new TcpClientWrapper(tcpClient);
+
+            // Act & Assert
+            // Cannot test without actual connection, but verify no exception
+            Assert.DoesNotThrow(() => wrapper.Dispose());
+        }
+
+        [Test]
+        public void Dispose_ShouldCloseClient()
+        {
+            // Arrange
+            var tcpClient = new TcpClient();
+            var wrapper = new TcpClientWrapper(tcpClient);
+
+            // Act & Assert
+            Assert.DoesNotThrow(() => wrapper.Dispose());
+        }
+    }
+}
